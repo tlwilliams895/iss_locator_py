22b9d51effcf60898f6bb7ae1a7f9834e6b07b19
tlwilliams895 <tlwilliams895@gmail.com>
2020-12-21 09:58:39 -0500

    Initial commit

--- iss.py
+++ iss.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = """TL Williams (tlwilliams895) completed assessment with
+            Deidre Boddie and Dessance Chandler."""
+
+import requests
+import turtle
+import time
+
+# Part A: Write a Python program to obtain a list of the astronauts who are
+# currently in space. Print their full names, the spacecraft they are currently
+# on board, and the total number of astronauts in space.
+
+def locate_astros():
 
 
 def main():
-    pass
+    iss_astros = locate_astros()
 
 
 if __name__ == '__main__':
