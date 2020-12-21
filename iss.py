5a7c3264bc86a60dc71f64947b33d3cb542c133a
tlwilliams895 <tlwilliams895@gmail.com>
2020-12-21 13:44:29 -0500

    update to assessment

--- iss.py
+++ iss.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = """TL Williams (tlwilliams895)
+            worked with Deidre Boddie and Dessance Changler;
+            received assistance from Tim La, Q4 Student/Mentor"""
+
+import requests
+import turtle
+import time
 
 
 def main():
